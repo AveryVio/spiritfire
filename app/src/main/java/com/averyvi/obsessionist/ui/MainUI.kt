cbcc9392e0a5112252fef8eaae5a6e63c5951baf
AveryVi <main.averyvi@gmail.com>
2026-04-11 23:14:43 +0200

    basic nav controller

--- app/src/main/java/com/averyvi/obsessionist/ui/MainUI.kt
+++ app/src/main/java/com/averyvi/obsessionist/ui/MainUI.kt
@@ -1,20 +1,51 @@
 package com.averyvi.obsessionist.ui
 
+import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material3.Scaffold
-import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
 import androidx.compose.ui.Modifier
+import androidx.navigation.NavHostController
+import androidx.navigation.compose.NavHost
+import androidx.navigation.compose.composable
+import androidx.navigation.compose.currentBackStackEntryAsState
+import androidx.navigation.compose.rememberNavController
+import com.averyvi.obsessionist.R
+import com.averyvi.obsessionist.ui.screens.RenameTileScreen
 
 @Composable
 fun MainUI(){
+
+    val navController: NavHostController = rememberNavController()
+    val navBackStackEntry by navController.currentBackStackEntryAsState()
+    val currentRoute = navBackStackEntry?.destination?.route
+
     Scaffold() { innerPadding ->
-        Box(){
-            Text(
-                text = "jgkldfsjgkl",
-                modifier = Modifier.padding(innerPadding)
-            )
+        Box(
+            modifier = Modifier.padding(innerPadding)
+        ) {
+            NavHost(
+                navController = navController,
+                startDestination = Routes.RenameTile.name
+            ) {
+                composable(route = Routes.Intro.name) {
+
+                }
+                composable(route = Routes.RenameTile.name) {
+                    RenameTileScreen()
+                }
+            }
         }
     }
+}
+
+enum class Routes(title: Int){
+    Intro(
+        title = R.string.IntroScreen
+    ),
+    RenameTile(
+        title = R.string.RenameTileScreen
+    ),
 }
---
+++ app/src/main/java/com/averyvi/obsessionist/ui/elements/textinput.kt
@@ -0,0 +1,64 @@
+package com.averyvi.obsessionist.ui.elements
+
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.foundation.text.KeyboardOptions
+import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.TextField
+import androidx.compose.material3.TextFieldDefaults
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.geometry.Offset
+import androidx.compose.ui.graphics.Brush
+import androidx.compose.ui.graphics.Color
+import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.TextStyle
+import androidx.compose.ui.text.rememberTextMeasurer
+import androidx.compose.ui.unit.TextUnit
+import androidx.compose.ui.unit.dp
+import androidx.compose.ui.unit.sp
+
+@Composable
+fun ObsTextInput(
+    label: @Composable () -> Unit,
+    placeholder: @Composable () -> Unit,
+    value: String,
+    onValueChange: (String) -> Unit,
+    modifier: Modifier = Modifier,
+    brushColorList: List<Color> = listOf( MaterialTheme.colorScheme.primary, MaterialTheme.colorScheme.secondary, MaterialTheme.colorScheme.tertiary ),
+    gradientTextStyle: TextStyle = TextStyle(fontSize = TextUnit.Unspecified),
+    keyboardOptions: KeyboardOptions = KeyboardOptions.Default,
+){
+    val textMeasurer = rememberTextMeasurer()
+    val textLayoutResult = textMeasurer.measure(
+        text = value,
+        style = gradientTextStyle
+    )
+    val gradientEndX = if (textLayoutResult.size.width > 0) {
+        textLayoutResult.size.width.toFloat()
+    } else {
+        1f
+    }
+
+    TextField(
+        label = label,
+        placeholder = placeholder,
+        value = value,
+        onValueChange = onValueChange,
+        modifier = modifier,
+        colors = TextFieldDefaults.colors(
+            focusedIndicatorColor = Color.Transparent,
+            unfocusedIndicatorColor = Color.Transparent,
+        ),
+        shape = RoundedCornerShape(24.dp),
+        textStyle = TextStyle().copy(
+            fontSize = 20.sp,
+            brush = Brush.linearGradient(
+                colors = brushColorList,
+                end = Offset(gradientEndX, 0f)
+            )
+        ),
+        keyboardOptions = keyboardOptions,
+        singleLine = true,
+    )
+}
